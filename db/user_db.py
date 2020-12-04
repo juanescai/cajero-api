4cccc81fa4d9b96b38309e0c8b3e384d349c061b
juanescai <jecaicedop@unal.edu.co>
2020-12-03 19:27:00 -0500

    Primer commit

---
+++ db/user_db.py
@@ -0,0 +1,27 @@
+from typing import Dict
+from pydantic import BaseModel
+
+class UserInDB(BaseModel):
+    username: str
+    password: str
+    saldo: int
+
+database_users = Dict[str,UserInDB]
+
+database_users = {"raul10":UserInDB(**{"username":"raul10",
+                                        "password":"mintic",
+                                        "saldo":15000}),
+                   "juan20":UserInDB(**{"username":"juan20",
+                                        "password":"mintic2020",
+                                        "saldo":10000})  
+                 }
+
+def get_user(username: str):
+    if username in database_users.keys():
+        return database_users[username]
+    else:
+        return None
+def update_user(user_in_db: UserInDB):
+    database_users[user_in_db.username] = user_in_db
+    return user_in_db
+
